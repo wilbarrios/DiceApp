2b75aa6753eae0725576ae42e40d6954b8125668
Wilmer Barrios <wilmerbt@yahoo.com>
2021-06-13 15:23:17 -0500

    `DiceAppUseCase` load succed presents dice

--- DiceAppCoreTests/DiceAppUseCaseTests.swift
+++ DiceAppCoreTests/DiceAppUseCaseTests.swift
@@ -8,7 +8,7 @@
 import Foundation
 import XCTest
 
-struct Dice {
+struct Dice: Equatable {
     let value: Int
 }
 
@@ -16,38 +16,100 @@ protocol DiceLoader {
     func getDice(diceCount: Int, completion: @escaping (Result<[Dice], Error>) -> Void)
 }
 
+protocol DiceView {
+    func present(_ dices: [Dice])
+}
+
+protocol ErrorView {
+    func present(errorMessage: String)
+}
+
 class DiceAppUseCase {
+    // MARK: Datasource
     private let loader: DiceLoader
+    
+    // MARK: Presentation
+    private let view: DiceView
+    private let errorView: ErrorView
+    
     private var DICE_COUNT: Int { 2 }
     
-    init(loader: DiceLoader) {
+    init(loader: DiceLoader, view: DiceView, errorView: ErrorView) {
         self.loader = loader
+        self.view = view
+        self.errorView = errorView
     }
     
     func loadDices() {
-        loader.getDice(diceCount: DICE_COUNT) {_ in }
+        loader.getDice(diceCount: DICE_COUNT) { [weak self] result in
+            if let dices = try? result.get() {
+                self?.view.present(dices)
+            }
+        }
     }
 }
 
 class DiceAppUseCaseTests: XCTestCase {
     func test_init_doesNotLoadDices() {
-        let (_, loader) = makeSUT()
+        let (_, loader, _, _) = makeSUT()
         XCTAssertEqual(loader.messagesCount, 0)
     }
     
     func test_loadDices_fetchDices() {
-        let (sut, loader) = makeSUT()
+        let (sut, loader, _, _) = makeSUT()
         sut.loadDices()
         
         XCTAssertEqual(loader.messagesCount, 1)
     }
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: DiceAppUseCase, loader: DiceLoaderMock) {
+    func test_loadDiceSucceed_presentsDice() {
+        let (sut, loader, view, _) = makeSUT()
+        
+        sut.loadDices()
+        let diceData = [makeDice(), makeDice()]
+        loader.complete(.success(diceData))
+        
+        XCTAssertEqual(view.presentedDices, diceData)
+    }
+    
+    // MARK: Helpers
+    
+    private func makeDice(value: Int = 2) -> Dice {
+        Dice(value: value)
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: DiceAppUseCase, loader: DiceLoaderMock, view: DiceViewMock, errorView: ErrorViewMock) {
         let loader = DiceLoaderMock()
-        let sut = DiceAppUseCase(loader: loader)
+        let view = DiceViewMock()
+        let errorView = ErrorViewMock()
+        let sut = DiceAppUseCase(loader: loader, view: view, errorView: errorView)
         trackMemoryLeaks(loader, file: file, line: line)
         trackMemoryLeaks(sut, file: file, line: line)
-        return (sut, loader)
+        trackMemoryLeaks(view, file: file, line: line)
+        trackMemoryLeaks(errorView, file: file, line: line)
+        return (sut, loader, view, errorView)
+    }
+    
+    // MARK: Testing Entities
+    
+    private class ErrorViewMock: ErrorView {
+        private var messages = [String]()
+        var callCount: Int { messages.count }
+        var presentedErrorMessage: String? { messages.last }
+        
+        func present(errorMessage: String) {
+            messages.append(errorMessage)
+        }
+    }
+    
+    private class DiceViewMock: DiceView {
+        private var messages = [[Dice]]()
+        var callCount: Int { messages.count }
+        var presentedDices: [Dice]? { messages.last }
+        
+        func present(_ dices: [Dice]) {
+            messages.append(dices)
+        }
     }
     
     private class DiceLoaderMock: DiceLoader {
@@ -57,6 +119,11 @@ class DiceAppUseCaseTests: XCTestCase {
         
         func getDice(diceCount: Int, completion: @escaping (Result<[Dice], Error>) -> Void) {
             messages.append(completion)
+        }
+        
+        // MARK: Helpers
+        func complete(_ result: Result<[Dice], Error>, index: Int = 0) {
+            messages[index](result)
         }
     }
 }
