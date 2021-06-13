c5e81f39c37618e00e15fff9a3d7f44a71154160
Wilmer Barrios <wilmerbt@yahoo.com>
2021-06-13 14:45:28 -0500

    `DiceAppCore` with test target added

---
+++ DiceAppCoreTests/DiceAppUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  DiceAppUseCaseTests.swift
+//  DiceAppCoreTests
+//
+//  Created by Wilmer Barrios on 13/06/21.
+//
+
+import Foundation
+import XCTest
+
+class DiceAppUseCaseTests: XCTestCase {
+    
+}
