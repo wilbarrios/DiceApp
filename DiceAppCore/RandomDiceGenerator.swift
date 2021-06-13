0f67bd43fe9e8df883648e80947852411089220c
Wilmer Barrios <wilmerbt@yahoo.com>
2021-06-13 16:40:22 -0500

    `RandomDiceGenerator` moved to its own file

---
+++ DiceAppCore/RandomDiceGenerator.swift
@@ -0,0 +1,39 @@
+//
+//  RandomDiceGenerator.swift
+//  DiceAppCore
+//
+//  Created by Wilmer Barrios on 13/06/21.
+//
+
+import Foundation
+
+class RandomDiceGenerator {
+    private let lowerBound: Int
+    private let higherBound: Int
+    
+    private static var LOWER_BOUND: Int { 1 }
+    private static var HIGHER_BOUND: Int { 7 }
+    
+    init(lowerBound: Int = LOWER_BOUND, higherBound: Int = HIGHER_BOUND) {
+        self.lowerBound = lowerBound
+        self.higherBound = higherBound
+    }
+    
+    func generate() -> Int {
+        Int.random(in: (lowerBound ... higherBound))
+    }
+}
+
+extension RandomDiceGenerator: DiceLoader {
+    func getDice(diceCount: Int, completion: @escaping (Result<[Dice], Error>) -> Void) {
+        var result = [Dice]()
+        for _ in (0..<diceCount) {
+            result.append(map(value: generate()))
+        }
+        completion(.success(result))
+    }
+    
+    private func map(value: Int) -> Dice {
+        Dice(value: value)
+    }
+}
--- DiceAppCoreTests/RandomDiceGeneratorUseCaseTests.swift
+++ DiceAppCoreTests/RandomDiceGeneratorUseCaseTests.swift
@@ -9,37 +9,6 @@ import Foundation
 import XCTest
 @testable import DiceAppCore
 
-class RandomDiceGenerator {
-    private let lowerBound: Int
-    private let higherBound: Int
-    
-    private static var LOWER_BOUND: Int { 1 }
-    private static var HIGHER_BOUND: Int { 7 }
-    
-    init(lowerBound: Int = LOWER_BOUND, higherBound: Int = HIGHER_BOUND) {
-        self.lowerBound = lowerBound
-        self.higherBound = higherBound
-    }
-    
-    func generate() -> Int {
-        Int.random(in: (lowerBound ... higherBound))
-    }
-}
-
-extension RandomDiceGenerator: DiceLoader {
-    func getDice(diceCount: Int, completion: @escaping (Result<[Dice], Error>) -> Void) {
-        var result = [Dice]()
-        for _ in (0..<diceCount) {
-            result.append(map(value: generate()))
-        }
-        completion(.success(result))
-    }
-    
-    private func map(value: Int) -> Dice {
-        Dice(value: value)
-    }
-}
-
 class RandomDiceGeneratorUseCaseTests: XCTestCase {
     func test_getOneDice_succeed() {
         assert(makeSUT(), dicesCount: 2)
