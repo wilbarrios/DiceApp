46d9b6a33b627a831039930d5772c5ba954de082
Wilmer Barrios <wilmerbt@yahoo.com>
2021-06-13 14:58:40 -0500

    `DiceAppUseCase` init does not load dices

--- DiceAppCoreTests/DiceAppUseCaseTests.swift
+++ DiceAppCoreTests/DiceAppUseCaseTests.swift
@@ -8,6 +8,43 @@
 import Foundation
 import XCTest
 
-class DiceAppUseCaseTests: XCTestCase {
+struct Dice {
+    let value: Int
+}
+
+protocol DiceLoader {
+    func getDice(completion: @escaping (Result<Dice, Error>) -> Void)
+}
+
+class DiceAppUseCase {
+    private let loader: DiceLoader
     
+    init(loader: DiceLoader) {
+        self.loader = loader
+    }
+}
+
+class DiceAppUseCaseTests: XCTestCase {
+    func test_init_doesNotLoadDices() {
+        let (_, loader) = makeSUT()
+        XCTAssertEqual(loader.messagesCount, 0)
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: DiceAppUseCase, loader: DiceLoaderMock) {
+        let loader = DiceLoaderMock()
+        let sut = DiceAppUseCase(loader: loader)
+        trackMemoryLeaks(loader, file: file, line: line)
+        trackMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
+    
+    private class DiceLoaderMock: DiceLoader {
+        private var messages = [(Result<Dice, Error>) -> Void]()
+        
+        var messagesCount: Int { messages.count }
+        
+        func getDice(completion: @escaping (Result<Dice, Error>) -> Void) {
+            messages.append(completion)
+        }
+    }
 }
